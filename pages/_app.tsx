7ac3ef3e69769b625dd78ef1f6ee7f9ea5c0d677
Basil Dimopoulos <basil.dimopoulos@gmail.com>
2022-11-19 15:17:07 +1030

    feat: improved blog template and added temp navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,42 @@
-import Nav from '../components/Nav'
+import NavBar from '../components/NavBar'
 import '../styles/globals.css'
 import Head from 'next/head'
+import localFont from '@next/font/local'
+
+const monaSans = localFont({
+  src:[
+    {
+      path: '../assets/fonts/Mona-Sans-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Mona-Sans-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Mona-Sans-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Mona-Sans-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Mona-Sans-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Mona-Sans-Black.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ]
+})
 
 export default function MyApp({ Component, pageProps }: any) {
   return (
@@ -8,9 +44,9 @@ export default function MyApp({ Component, pageProps }: any) {
       <Head>
         <title>Next and MDX Blog</title>
       </Head>
-      <div className="container">
-        <Nav />
-        <main>
+      <div>
+        <NavBar />
+        <main className='monaSans.className'>
           <Component {...pageProps} />
         </main>
       </div>
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,39 +7,47 @@ import SyntaxHighlighter from 'react-syntax-highlighter'
 import Button from '../../components/Button.tsx'
 
 export const getStaticPaths = async () => {
-    const files = fs.readdirSync(path.join('posts'))
-    const paths = files.map(filename => ({
-      params: {
-        slug: filename.replace('.mdx', '')
-      }
-    }))
-    return {
-      paths,
-      fallback: false
+  const files = fs.readdirSync(path.join('posts'))
+  const paths = files.map(filename => ({
+    params: {
+      slug: filename.replace('.mdx', '')
     }
+  }))
+  return {
+    paths,
+    fallback: false
+  }
 }
 
 export const getStaticProps = async ({ params: { slug } }) => {
-    const markdownWithMeta = fs.readFileSync(path.join('posts',
-      slug + '.mdx'), 'utf-8')
-    const { data: frontMatter, content } = matter(markdownWithMeta)
-    const mdxSource = await serialize(content)
-    return {
-      props: {
-        frontMatter,
-        slug,
-        mdxSource
-      }
+  const markdownWithMeta = fs.readFileSync(path.join('posts',
+    slug + '.mdx'), 'utf-8')
+  const { data: frontMatter, content } = matter(markdownWithMeta)
+  const mdxSource = await serialize(content)
+  return {
+    props: {
+      frontMatter,
+      slug,
+      mdxSource
     }
+  }
 }
 
-const PostPage = ({ frontMatter: { title }, mdxSource }) => {
-    return (
-      <div className="mt-4">
-        <h1>{title}</h1>
+const PostPage = ({ frontMatter: { title, description, thumbnailUrl }, mdxSource }) => {
+  return (
+    <div>
+      <div className="pt-20 post-background-header">
+        <div className='flex flex-col items-center'>
+          <h1 className='h1-post text-center'>{title}</h1>
+          <p className="p-post-description text-center mt-6 mb-10">{description}</p>
+          <img src={thumbnailUrl}></img>
+        </div>
+      </div>
+      <div className='bg-white'>
         <MDXRemote {...mdxSource} components={{ Button, SyntaxHighlighter }} />
       </div>
-    )
+    </div>
+  )
 }
 
 export default PostPage
