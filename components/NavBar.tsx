0ba9e730c40104042cf17c3e834f109894bee44b
Basil Dimopoulos <basil.dimopoulos@gmail.com>
2022-12-02 14:28:08 +1030

    feat: added tags to blog

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,13 +13,13 @@ export default function NavBar() {
                 </div> */}
 
             </div>
-            <div className="flex items-center space-x-4">
+            {/* <div className="flex items-center space-x-4">
                 <ul className="flex items-center font-semibold space-x-4 max-sm:hidden">
                     <li><a href="#" className="hover:text-gray-400">LATEST BLOGS</a></li>
                     <li><a href="#" className="hover:text-gray-400">ABOUT</a></li>
                     <li><a href="#" className="hover:text-gray-400">CONTACT</a></li>
                 </ul>
-            </div>
+            </div> */}
         </nav>
     )
 }
