4bf2fe42f63e5da513df36d24eadb7f1068274cd
Basil Dimopoulos <basil.dimopoulos@gmail.com>
2023-01-14 09:38:33 +1030

    feat: added new landing section and demo of potfolio

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -1,8 +1,13 @@
 export default function LandingSection() {
     return (
-        <div className="h-[75vh] flex flex-col items-center text-center justify-center bg-[url('/images/repeated-square-dark.webp')]">
-            <h1 className="w-auto leading-[52px] text-[48px] md:text-[72px] md:leading-[80px] text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-red-500">Hello World</h1>
-            <p className="mt-3 px-5 text-white">Welcome to the other side. The journey of your developer experience is boosted here.</p>
+        <div className="w-full h-[880px] flex">
+            <div className="bg-[#201E1E]">
+                <div className="p-20 h-full flex flex-col justify-center">
+                    <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl tracking-tight">Normal tutorials make you look like this</h1>
+                    <p className="mt-6 text-lg text-slate-400 max-w-3xl">Meet Felu. Your new go to story time for the learned developer.</p>
+                </div>
+            </div>
+            <div className="w-full h-full bg-[url(/images/felu-landing-comic.jpg)] bg-cover bg-center" />
         </div>
     )
 }
---
+++ pages/portfolio.tsx
@@ -0,0 +1,15 @@
+export default function PortfolioPage() {
+    return (
+        <div>
+            <div className="w-full h-[870px] flex justify-center cropped bg-slate-900">
+                <div className="w-full">
+                    <h1>HAS CODING HURT YOU?</h1>
+                    <p>It has to me. The look of a person reading through the junior developer stack. Yes I subscribed to Adobe Stock for this meme.</p>
+                </div>
+                <div className="w-full overflow-hidden relative">
+                    <img className="w-full top-0 right-0 absolute" src="/images/portfolio-landing.jpg"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
