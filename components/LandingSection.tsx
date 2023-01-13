124066cb0d41e1a1ace38f13b55ea8d1e19a9bb3
Basil Dimopoulos <basil.dimopoulos@gmail.com>
2023-01-14 10:22:28 +1030

    feat: made landing page responsive and added new fonts

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -1,13 +1,13 @@
 export default function LandingSection() {
     return (
-        <div className="w-full h-[880px] flex">
-            <div className="bg-[#201E1E]">
-                <div className="p-20 h-full flex flex-col justify-center">
-                    <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl tracking-tight">Normal tutorials make you look like this</h1>
-                    <p className="mt-6 text-lg text-slate-400 max-w-3xl">Meet Felu. Your new go to story time for the learned developer.</p>
+        <div className="w-full h-[800px] sm:h-[920px] flex">
+            <div className="bg-[#201E1E] w-full">
+                <div className="p-12 md:p-20 h-full flex flex-col justify-center font-normal">
+                    <h1 className="text-white font-anton text-[32px] leading-10 md:text-[65px] md:leading-[68px] uppercase">Normal tutorials make you scream</h1>
+                    <p className="mt-3 md:mt-6 text-lg text-slate-300 max-w-3xl">Meet Felu. Your new go to story time for the learned developer.</p>
                 </div>
             </div>
-            <div className="w-full h-full bg-[url(/images/felu-landing-comic.jpg)] bg-cover bg-center" />
+            <div className="md:w-full h-full bg-[url(/images/felu-landing-comic.jpg)] bg-cover bg-center" />
         </div>
     )
 }
