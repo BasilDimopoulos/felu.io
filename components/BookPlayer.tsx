b24029d9eea7f88e8455af0d594ca687162ffb1a
Basil Dimopoulos <basil.dimopoulos@gmail.com>
2023-01-24 18:52:29 +1030

    added initial funcitonality to bookplayer

--- components/BookPlayer.tsx
+++ components/BookPlayer.tsx
@@ -3,8 +3,32 @@ import BookImageComponent from "./BookImageComponent";
 import BookTextComponent from "./BookTextComponent";
 import TextData from "../public/json_files/demo-text.json"
 
+class Page {
+    type: String;
+    value: String[] | String;
+
+    constructor(type: string, value: String[] | String) {
+        this.type = type;
+        this.value = value;
+    }
+}
+
+class Book {
+    pages: Page[];
+    pageLength: Number;
+    readingLocation: Number;
+
+    constructor(pages: Page[]) {
+        this.pages = pages;
+        this.pageLength = pages.length;
+        this.readingLocation = 1;
+    }
+}
+
 export default function BookPlayer() {
-    console.log(TextData.value)
+    var curentLocation = 1;
+    let pages = [new Page(TextData.type, TextData.value), new Page(TextData.type, TextData.value)];
+    let chapter: Book = new Book(pages);
     return (
         <div className="w-full h-[920px]  pl-44 pr-44 pt-16 pb-16">
             <div className="w-full bg-slate-500 h-1 mb-5"></div>
@@ -12,7 +36,7 @@ export default function BookPlayer() {
                 {/* container 1 */}
                 <div className="flex-1 flex flex-col justify-center">
                     <div className="w-10/12">
-                        <BookTextComponent text={TextData.value} />
+                        <BookTextComponent text={chapter.pages[0].value} />
                     </div>
                 </div>
                 {/* container 2 */}
