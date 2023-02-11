791511d322db92d737438215458705a4d3e1e214
Basil <basil.dimopoulos@dewc.com>
2023-02-11 16:52:13 +1030

    feat: added new component to home page

--- components/HomePageComponent.tsx
+++ components/HomePageComponent.tsx
@@ -5,24 +5,24 @@ export default function HomePageComponent() {
     <div className="container-xl">
       <div className="flex h-[850px]">
         <div className="w-full flex flex-col justify-center">
-          <h2 className="uppercase font-anton grow-">
-            A little coding goes a long way{" "}
-          </h2>
-          <p>
-            Meet Felu. Your new go to story time for the learned developer. Meet
-            Felu. Your new go to story time for the learned developer. Meet
-            Felu. Your new go to story time for the learned developer.
-          </p>
-          <Button className="w-28">Click Me</Button>
+          <div className="max-w-xl">
+            <h2 className="uppercase font-anton text-5xl font-bold mb-2">
+              A little coding goes a long way
+            </h2>
+            <p className="text-xl leading-7 mb-5">
+              Meet Felu. Your new go to story time for the learned developer. Some other text that I dont know what to put goes here.
+            </p>
+            <button className="bg-[#1E222A] text-white pt-4 pb-4 w-40 text-xl tracking-wide font-anton uppercase">Click Me</button>
+          </div>
         </div>
 
         <div className="w-full h-full flex justify-start">
           <img
             alt="home image"
             src="/images/home_1.jpg"
-            className="h-full z-10"
+            className="w-auto z-10 pt-10 pb-10"
           ></img>
-          <div className="bg-[#1D1B20] absolute h-[850px] w-1/4 right-0"></div>
+          <div className="bg-[#1D1B20] absolute h-[850px] w-[33%] right-0"></div>
         </div>
       </div>
     </div>
