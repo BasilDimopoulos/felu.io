fd4e1aa0ca21811d26715ddde708d3c22ba9debf
Basil <basil.dimopoulos@dewc.com>
2023-02-13 22:36:45 +1030

    feat: fixed blog design

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -38,16 +38,16 @@ const PostPage = ({ frontMatter: { title, description, tags, thumbnailUrl }, mdx
     document.title = title;
   }, []);
   return (
-    <div className='w-full bg-[#22272e]'>
+    <div className='w-full bg-[#22272e] pt-24 md:pt-20'>
       <div className='container-xl'>
         <div className='lg:p-24 p-8'>
           <div className='flex'>
             {tags.map(tag => (<p className='pr-5 text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-red-500' key={tag}>{tag}</p>))}
             {/* <p>{tags}</p> */}
           </div>
-          <h1 className='mt-2 w-full text-[32px] leading-[36px] md:text-[42px] md:leading-[48px] lg:text-[48px] lg:leading-[52px]'>{title}</h1>
+          <h1 className='mt-2 text-white font-anton uppercase w-full text-4xl md:text-5xl'>{title}</h1>
           <p className="post-description mt-3 mb-16">{description}</p>
-          <img src={thumbnailUrl}></img>
+          <img alt="image_thumbnail" src={thumbnailUrl}></img>
         </div>
       </div>
       <div className='w-full bg-[#ececec]'>
